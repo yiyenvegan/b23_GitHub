c66af80fae3835e409942147a7aac0ae6dc9d774
yiyenvegan <halilyildirim1908@gmail.com>
2021-07-20 10:35:53 -0400

    added new commits

--- src/GitCool.java
+++ src/GitCool.java
@@ -2,6 +2,7 @@ public class GitCool {
 
     public static void main(String[] args) {
         System.out.println("hello github.com");
+        System.out.println("we have added new comiits");
 
     }
 }
